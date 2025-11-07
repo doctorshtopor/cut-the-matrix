032978127cf17686fa6601ffdfe70f6b7c345053
Grigorij Zakharov <kapustin.gd23@physics.msu.ru>
2025-11-07 16:36:57 +0300

    The number of rows and columns per block is calculated

--- program.py
+++ program.py
@@ -5,5 +5,29 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+M, N = 10, 10
+k1, k2 = 2, 3
+assert k1 * k2 == size, "numprocs != k1*k2"
 
-print(rank)
+
+if rank == 0:
+    A = np.arange(1, M * N + 1, dtype=np.int32).reshape(M, N)
+    print("Матрица:\n", A, flush=True)
+else:
+    A = None
+
+
+rows_per_block = np.zeros(k1, dtype=np.int32)
+cols_per_block = np.zeros(k2, dtype=np.int32)
+
+#распределяю сколько строк будет в каждом блоке
+for i in range(k1):
+    rows_per_block[i] = M // k1
+    if i < M % k1:  #остаток строк делю между первыми блоками
+        rows_per_block[i] += 1
+
+#для столбцов аналогично
+for j in range(k2):
+    cols_per_block[j] = N // k2
+    if j < N % k2:  
+        cols_per_block[j] += 1
